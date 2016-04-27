e692e92629d8b8ffb91555b9806c087c04968f4e
tchiotludo <tchiot.ludo@gmail.com>
2016-04-27 16:11:24 +0200

    FormSelect : control value & remove empty option for required value

--- src/Forms/Fields/Select.php
+++ src/Forms/Fields/Select.php
@@ -18,6 +18,11 @@ use Cawa\Renderer\HtmlElement;
 class Select extends AbstractField
 {
     /**
+     * @var array
+     */
+    private $options = [];
+
+    /**
      * @param string $name
      * @param string $label
      * @param array $options
@@ -25,13 +30,14 @@ class Select extends AbstractField
     public function __construct(string $name, string $label = null, array $options = [])
     {
         parent::__construct('<select>', $name, $label);
+        $this->options = $options;
 
-        $isAssociative = array_keys($options) !== range(0, count($options) - 1);
+        $isAssociative = array_keys($this->options) !== range(0, count($this->options) - 1);
 
         $option = new HtmlElement('<option>');
         $this->getField()->add($option);
 
-        foreach ($options as $key => $value) {
+        foreach ($this->options as $key => $value) {
             $option = new HtmlElement('<option>');
             $option->setContent((string) $value);
             $option->addAttribute('value', $isAssociative ? (string) $key : (string) $value);
@@ -43,8 +49,25 @@ class Select extends AbstractField
     /**
      * {@inheritdoc}
      */
+    public function setRequired(bool $required = true)
+    {
+        array_shift($this->getField()->elements);
+        return parent::setRequired($required);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     public function setValue($value) : parent
     {
+        if (!in_array($value, array_keys($this->options))) {
+            throw new \InvalidArgumentException(sprintf(
+                "Invalid option value '%s' for select '%s'",
+                $value,
+                $this->getName()
+            ));
+        }
+
         /** @var HtmlElement $element */
         foreach ($this->getField()->elements as $element) {
             if ($element->getAttribute('value') == $value) {
