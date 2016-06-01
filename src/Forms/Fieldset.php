850fbee923cfd7459b24644a055e61f6412761d8
tchiotludo <tchiot.ludo@gmail.com>
2016-06-01 14:53:22 +0200

    Fieldset Legend

--- src/Forms/Fieldset.php
+++ src/Forms/Fieldset.php
@@ -15,15 +15,20 @@ namespace Cawa\Html\Forms;
 
 use Cawa\Html\Forms\Fields\File;
 use Cawa\Renderer\HtmlContainer;
+use Cawa\Renderer\HtmlElement;
 
 class Fieldset extends HtmlContainer
 {
     /**
-     *
+     * @param string $legend
      */
-    public function __construct()
+    public function __construct(string $legend = null)
     {
         parent::__construct('<fieldset>');
+
+        if ($legend) {
+            $this->add(HtmlElement::create('<legend>', $legend));
+        }
     }
 
     /**
@@ -31,6 +36,10 @@ class Fieldset extends HtmlContainer
      */
     public function getFields() : array
     {
+        if ($this->elements[0]->getTag() == '<legend>') {
+            return array_slice($this->elements, 1);
+        }
+
         return $this->elements;
     }
 }
