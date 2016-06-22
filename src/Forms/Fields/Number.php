995c12041bda8ca48b6205efefda2461b12321e4
tchiotludo <tchiot.ludo@gmail.com>
2016-06-22 17:48:06 +0200

    Form Number : html5 attributes & float handle

--- src/Forms/Fields/Number.php
+++ src/Forms/Fields/Number.php
@@ -23,5 +23,72 @@ class Number extends AbstractField
     {
         parent::__construct('<input />', $name, $label);
         $this->getField()->addAttribute('type', 'number');
+        $this->setValidation('int');
+    }
+
+    /**
+     * @return float
+     */
+    public function getMin()
+    {
+        return $this->getField()->getAttribute('min');
+    }
+
+    /**
+     * @param float $min
+     *
+     * @return $this
+     */
+    public function setMin(float $min) : self
+    {
+        $this->getField()->addAttribute('min', (string) $min);
+
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getMax()
+    {
+        return $this->getField()->getAttribute('max');
+    }
+
+    /**
+     * @param float $max
+     *
+     * @return $this
+     */
+    public function setMax(float $max) : self
+    {
+        $this->getField()->addAttribute('max', (string) $max);
+
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getStep()
+    {
+        return $this->getField()->getAttribute('step');
+    }
+
+    /**
+     * @param float $step
+     *
+     * @return $this
+     */
+    public function setStep(float $step) : self
+    {
+        if ($step >= 1) {
+            $this->setValidation('int');
+        } else {
+            $this->setValidation('float');
+        }
+
+        $this->getField()->addAttribute('step', (string) $step);
+
+        return $this;
     }
 }
