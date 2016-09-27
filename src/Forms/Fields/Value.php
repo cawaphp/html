3c58f4ee3f70c59ad4b0cdd5a47b3a62cc50e36e
tchiotludo <tchiot.ludo@gmail.com>
2016-09-27 17:19:25 +0200

    Form : Value Field for dynamic form

---
+++ src/Forms/Fields/Value.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the Сáша framework.
+ *
+ * (c) tchiotludo <http://github.com/tchiotludo>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare (strict_types=1);
+
+namespace Cawa\Html\Forms\Fields;
+
+class Value extends Hidden
+{
+    /**
+     * @param string $name
+     * @param string $value
+     */
+    public function __construct(string $name, string $value = null)
+    {
+        parent::__construct($name, $value);
+        $this->setRenderable(false);
+    }
+
+}
