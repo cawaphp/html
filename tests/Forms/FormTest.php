c59ef84393f05834b3aa5e77b8d9b7425062c48f
tchiotludo <tchiot.ludo@gmail.com>
2017-11-08 11:42:26 +0100

    update to phpunit 6.4

--- tests/Forms/FormTest.php
+++ tests/Forms/FormTest.php
@@ -23,7 +23,7 @@ use Cawa\App\HttpFactory;
 use Cawa\Html\Forms\Fields\AbstractField;
 use Cawa\Html\Forms\Fields\Text;
 use Cawa\Html\Forms\Form;
-use PHPUnit_Framework_TestCase as TestCase;
+use PHPUnit\Framework\TestCase;
 
 class FormTest extends TestCase
 {
